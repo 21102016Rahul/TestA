4a68269799900b491acb57546fc581e839e763fc
Babar <2460463@cognizant.com>
2026-01-20 11:10:51 +0530

    change in script

--- check_ip.sh
+++ check_ip.sh
@@ -1,4 +1,8 @@
-echo '#!/bin/bash
+cat << 'EOF' > check_ip.sh
+#!/bin/bash
 echo "Fetching IP address..."
-hostname -I | awk "{print \$1}"' > check_ip.sh
+# Print the first non-loopback IPv4 address
+hostname -I | awk '{print $1}'
+EOF
+
 chmod +x check_ip.sh
